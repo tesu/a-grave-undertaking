0788832ec5219d968767f9dc93f9d041df2c427f
Giorgi Tskhadadze <giotskhada@gmail.com>
2018-02-21 16:42:55 -0400

    Added a raycaster to "interact" with the board.

---
+++ A Grave Undertaking/Assets/Scripts/Raycaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Raycaster : MonoBehaviour {
+
+    private GraphicRaycaster raycaster;
+    private PointerEventData pointerEventData;
+    private EventSystem eventSystem;
+
+    void Start() {
+        raycaster = GetComponent<GraphicRaycaster>();
+        eventSystem = GetComponent<EventSystem>();
+    }
+
+    void Update() {
+
+    }
+
+    public List<RaycastResult> GetAllRaycastObjects() {
+        pointerEventData = new PointerEventData(eventSystem);
+
+        pointerEventData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        raycaster.Raycast(pointerEventData, results);
+
+        return results;
+    }
+}
