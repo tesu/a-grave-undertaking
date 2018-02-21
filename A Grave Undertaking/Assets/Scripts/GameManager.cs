718df322d3aa0b07d14e7c064dff4ad0481d027c
tesu <jasonlam@mit.edu>
2018-02-21 14:48:41 -0500

    attempted to get some boilerplate script code working

---
+++ A Grave Undertaking/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private int boardSize;
+
+	// Use this for initialization
+	void Start () {
+        for (int i=0; i<boardSize*boardSize; i++) {
+            GameObject cell = Instantiate(cellPrefab);
+            //cell.transform.SetParent(boardpanel.transform);
+            //board.addcell(cell)
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
