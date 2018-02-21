4c72012acc68e90e1c32ea728c2b8dbebece74a5
Giorgi Tskhadadze <giotskhada@gmail.com>
2018-02-21 16:08:57 -0400

    Added a script to automatically add squares on the board.

--- A Grave Undertaking/Assets/Scripts/GameManager.cs
+++ A Grave Undertaking/Assets/Scripts/GameManager.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    private int boardSize;
+
+    public GameObject WhiteCellPrefab;
+    public GameObject BlackCellPrefab;
+    public GameObject BoardPanel;
+
+    private int boardSize = 8;
 
 	// Use this for initialization
 	void Start () {
-        for (int i=0; i<boardSize*boardSize; i++) {
-            GameObject cell = Instantiate(cellPrefab);
-            //cell.transform.SetParent(boardpanel.transform);
-            //board.addcell(cell)
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                GameObject cell;
+                if ((i + j%2) % 2 == 0) {
+                    cell = Instantiate(WhiteCellPrefab);
+                }
+                else {
+                    cell = Instantiate(BlackCellPrefab);
+                }
+                cell.transform.SetParent(BoardPanel.transform);
+                //board.addcell(cell)
+            }
         }
 	}
 
